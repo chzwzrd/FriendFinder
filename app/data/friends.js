49922f910d791db76220c67a3922568a59c4032e
Melodie Chi <Melodie@Apple.local>
2017-12-18 17:22:07 -0800

    compare user's scores with each existing friend & find total difference for each

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friends = [
         name: 'Melodie',
         photo: 'https://avatars2.githubusercontent.com/u/20732445?s=460&v=4',
         scores: [
-            '3',
-            '2',
-            '3',
-            '1',
-            '3',
-            '1',
-            '3',
-            '2',
-            '3',
-            '1'
+            3,
+            2,
+            3,
+            1,
+            3,
+            1,
+            3,
+            2,
+            3,
+            1
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,9 +13,31 @@ router.get('/friends', (req, res) => {
 
 router.post('/friends', (req, res) => {
     var newFriend = req.body;
+
+    // convert user's scores from string to number
+    newFriend.scores = newFriend.scores.map(Number);
+
+    compareScores(newFriend);
+
     friends.push(newFriend);
+
     res.json(newFriend);
 });
+
+// FUNCTIONS
+// =====================================================================================
+// compare user's scores with each existing friend & find total difference in scores for each
+function compareScores(user) {
+    var friendResults = [];
+    for (var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
+        for (var j = 0; j < user.scores.length; j++) {
+            totalDifference += Math.abs(user.scores[j] - friends[i].scores[j]);
+        }
+        friendResults.push([friends[i].name, friends[i].photo, totalDifference]);
+    }
+    console.log(friendResults);
+}
 
 // EXPORT
 // =====================================================================================
