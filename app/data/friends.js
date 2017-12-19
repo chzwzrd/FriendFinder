7618c92f14e7a7e5dfafc8ec48692b6d05f0c4f5
Melodie Chi <Melodie@Apple.local>
2017-12-18 18:00:36 -0800

    find match and send back to DOM as post response!

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,22 @@ var friends = [
             3,
             1
         ]
+    },
+    {
+        name: 'Jack',
+        photo: 'img',
+        scores: [
+            5,
+            3,
+            2,
+            4,
+            1,
+            1,
+            2,
+            2,
+            4,
+            5
+        ]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,26 +17,60 @@ router.post('/friends', (req, res) => {
     // convert user's scores from string to number
     newFriend.scores = newFriend.scores.map(Number);
 
-    compareScores(newFriend);
+    // console insight
+    console.log();
+    console.log(newFriend);
 
+    // find which friend most compatible (smallest total difference in scores)
+    var friendMatch = compareScores(newFriend);
+
+    // push newFriend to friends list only after comparing scores
+    // because don't want to compare the user with his/her data when looping through friends array
     friends.push(newFriend);
 
-    res.json(newFriend);
+    // send back info (name, photo, totalDifference) of most compatible friend
+    res.json(friendMatch);
 });
 
 // FUNCTIONS
 // =====================================================================================
 // compare user's scores with each existing friend & find total difference in scores for each
 function compareScores(user) {
+
+    var friendInfo = [];
     var friendResults = [];
     for (var i = 0; i < friends.length; i++) {
         var totalDifference = 0;
         for (var j = 0; j < user.scores.length; j++) {
             totalDifference += Math.abs(user.scores[j] - friends[i].scores[j]);
         }
-        friendResults.push([friends[i].name, friends[i].photo, totalDifference]);
+        friendInfo.push([friends[i].name, friends[i].photo], totalDifference);
+        friendResults.push(totalDifference);
     }
+
+    console.log('====================');
+    console.log(friendInfo);
+    console.log('====================');
     console.log(friendResults);
+    console.log();
+
+    var friendMatchIndex = findMinIndex(friendResults);
+    return friendInfo[friendMatchIndex];
+
+}
+
+// https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array
+function findMinIndex(arr) {
+    var min = arr[0];
+    var minIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+            minIndex = i;
+        }
+    }
+    return minIndex;
 }
 
 // EXPORT
